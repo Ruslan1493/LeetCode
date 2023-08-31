77c6e483be6f6cad67cbbf079724f76fa5509b58
Ruslan Sagitov <r.irekovich@itsoft.dev>
2023-08-31 15:06:23 +0300

    addidng new auth

---
+++ notes-project/backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const { logger } = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
+const PORT = process.env.PORT | 3500;
+
+app.use(logger);
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', require('./routes/root'));
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not found' });
+    } else {
+        res.type('txt').send('404 Not found');
+    }
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
